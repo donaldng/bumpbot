bc3b89051f096b440f48608d11af1056e8d66b0c
donaldng <donaldyann@gmail.com>
2018-03-31 16:46:05 +0800

    Added initial function call

--- src/sqlschema.py
+++ src/sqlschema.py
@@ -2,28 +2,10 @@ from environment.config import *
 import sqlite3, records
 
 def createDB():
-	db = records.Database('sqlite:///{}'.format(dbname))
+    db = records.Database('sqlite:///{}'.format(dbname))
 
-	db.query("""
-	CREATE TABLE `user` (
-		`user_id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-		`username`	TEXT UNIQUE,
-		`password`	TEXT,
-		`firsttime`	INTEGER DEFAULT 1,
-		`created_at`	DATETIME DEFAULT 0
-	);
-	CREATE TABLE `post` (
-		`post_id`	INTEGER,
-		`title`	TEXT,
-		`url`	TEXT,
-		`count`	INTEGER DEFAULT 0,
-		`status`	INTEGER DEFAULT 1,
-		`deleted`	INTEGER DEFAULT 0,
-		`user_id`	INTEGER,
-		`next_execution`	INTEGER DEFAULT 0,
-		`created_at`	INTEGER,
-		`updated_at`	INTEGER,
-		`last_bump`	INTEGER DEFAULT 0,
-		PRIMARY KEY(`post_id`)
-	);
-	""")
+    db.query("CREATE TABLE `user` (`user_id`	INTEGER PRIMARY KEY AUTOINCREMENT,`username`	TEXT UNIQUE,`password`	TEXT,`firsttime`	INTEGER DEFAULT 1,`created_at`	DATETIME DEFAULT 0);")
+    db.query("CREATE TABLE `post` (`post_id`	INTEGER,`title`	TEXT,`url`	TEXT,`count`	INTEGER DEFAULT 0,`status`	INTEGER DEFAULT 1,`deleted`	INTEGER DEFAULT 0,`user_id`	INTEGER,`next_execution`	INTEGER DEFAULT 0,`created_at`	INTEGER,`updated_at`	INTEGER,`last_bump`	INTEGER DEFAULT 0,PRIMARY KEY(`post_id`));")
+
+if __name__ == "__main__":
+    createDB()
